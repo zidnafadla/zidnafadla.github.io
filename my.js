9a0832266097b0d6fb4d4d21294057be9146e75a
zidnafadla <zidna.me@gmail.com>
2020-10-14 09:40:24 +0700

    initial commit

--- my.js
+++ my.js
@@ -4,3 +4,24 @@ $('.btn-to-work').click(function (e) {
         scrollTop: $('#work').offset().top
     }, 2000);
 });
+
+
+$('.btnCS').on('click', function () {
+
+    id = $(this).attr('data-id');
+
+    const modal = $('.' + id);
+
+    modal.addClass('modal-active');
+});
+
+$('.btnl').on('click', function () {
+    const btn = $(this).attr('data-action');
+    if (btn == 'close') {
+        $(".modal-bg").removeClass('modal-active');
+    }
+});
+
+$(".btnGo").on('click', function () {
+    window.location.href = $(this).attr("action-href");
+});
